c10d46b2cfae04cdc2874d2671ddd1c6d006d731
sherazahra <sherazahra30@gmail.com>
2023-11-15 21:55:30 +0700

    tambah jwt

---
+++ index.js
@@ -0,0 +1,33 @@
+const express = require('express')
+const app = express()
+const port = 2000
+const cors = require('cors')
+
+app.use(cors())
+
+const bodyPs = require('body-parser');
+app.use(bodyPs.urlencoded({ extended: false}));
+app.use(bodyPs.json());
+
+app.get('/', (req, res) => {
+    res.send("Halo Shera!")
+});
+
+const pelangganRouter = require('./routes/pelanggan');
+app.use('/api/pelanggan', pelangganRouter);
+
+const kasirRouter = require('./routes/kasir');
+app.use('/api/kasir', kasirRouter);
+
+const menuRouter = require('./routes/menu');
+app.use('/api/menu', menuRouter);
+
+const bayarRouter = require('./routes/bayar');
+app.use('/api/bayar', bayarRouter);
+
+const auth = require('./routes/auth/auth');
+app.use('/api/auth', auth);
+
+app.listen(port, () => {
+    console.log(`aplikasi berjalan di http:://localhost:${port}`)
+})
